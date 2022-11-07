57ac9c119e36214f18f1227c97ebcdf6cbf08cae
khanhnguyen <khanh.nguyen@intrepid.asia>
2022-11-08 05:43:48 +0700

    Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
+import netlify from '@astrojs/netlify/functions';
 
 export default defineConfig({
   integrations: [react(), tailwind(
     {config: { applyBaseStyles: false }}
-  ), image()]
+  ), image()],
+  output: "server",
+  adapter: netlify(),
 });
